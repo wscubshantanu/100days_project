733cc0b553b36dbc3d66b2c4c7370f8bbfa08399
wscubshantanu <sandy082005@gmail.com>
2024-10-26 12:40:13 +0530

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -1,43 +1,26 @@
 from turtle import Turtle
 
-ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
-
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
-        self.high_score = 0
         self.color('white')
         self.penup()
-        self.goto(0, 270)
         self.hideturtle()
-        self.load_high_score()
+        self.l_score = 0
+        self.r_score = 0
+        self.goto(-100,200)
         self.update_scoreboard()
 
-    def load_high_score(self):
-        try:
-            with open("data.txt") as data:
-                self.high_score = int(data.read())
-        except FileNotFoundError:
-            self.high_score = 0
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f'Score: {self.score}  High Score: {self.high_score}', align=ALIGNMENT, font=FONT)
+        self.write(self.l_score, align='center', font=("courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align='center', font=("courier", 80, "normal"))
 
-    def increase_score(self):
-        self.score += 1
+    def l_point(self):
+        self.l_score += 1
         self.update_scoreboard()
 
-    def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-            with open('data.txt', mode='w') as data:
-                data.write(f"{self.high_score}")
-        self.score = 0
+    def r_point(self):
         self.update_scoreboard()
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
